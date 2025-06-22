28b728afe1d776ab742888b22df8f01c903bec3d
GBG7 <dryeeti4@gmail.com>
2025-06-21 20:03:55 -0400

    minhaz has 2 male lovers

---
+++ acp/client.py
@@ -0,0 +1,23 @@
+import asyncio
+from acp_sdk.client import Client
+from colorama import Fore
+
+async def run_hospital_workflow() -> None:
+    async with Client(base_url="http://localhost:8001") as insurer, Client(base_url="http://localhost:8000") as hospital:
+        run1 = await hospital.run_sync(
+            agent="health_agent", input="Do I need rehabilitation after a shoulder reconstruction? "
+                                        "Do not tell me to consult with medical professionals for personalized"
+                                        "advice. Give general advice that will not be taken as fact, and will be "
+                                        "verified."
+        )
+        print(run1)
+        content = run1.output[0].parts[0].content
+        print(Fore.LIGHTMAGENTA_EX+ content + Fore.RESET)
+
+        run2 = await insurer.run_sync(
+            agent="policy_agent", input=f"Context: {content} What is the waiting period for rehabilitation?"
+        )
+        print(Fore.YELLOW + run2.output[0].parts[0].content + Fore.RESET)
+
+if __name__ == "__main__":
+    asyncio.run(run_hospital_workflow())
